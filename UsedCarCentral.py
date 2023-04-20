e4cb9c933f96e46fe390afe053ffd7171405ffd7
ramkiran55 <ramkiran55.devireddy@gmail.com>
2023-04-20 06:46:14 -0400

    db-asci3

--- UsedCarCentral.py
+++ UsedCarCentral.py
@@ -6,11 +6,13 @@ UsedCarCentral = Flask(__name__)
 
 @UsedCarCentral.route("/")
 def main():
+    car_listings = []
     con = connection_uri()
     cursor = con.cursor()
     cursor.execute("SELECT TOP (5) * FROM real.CarListings")
     for row in cursor.fetchall():
-        print(row)
+        #print(row) 
+        car_listings.append({"id": row[0], "ListingID": row[1]})
     return render_template("CarsList.html")
 
 if __name__ == "__main__":
