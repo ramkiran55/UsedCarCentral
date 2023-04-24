5a5735488342e76560f748191934e30c82a972ab
ramkiran55 <ramkiran55.devireddy@gmail.com>
2023-04-24 01:10:31 -0400

    create-crud

--- models/db_models.py
+++ models/db_models.py
@@ -4,3 +4,13 @@ def insertCarListing(car_listing):
     con = connection.connection_uri()
     cursor = con.cursor()
     
+    """
+    Flow of inserting into Car Listings
+    
+     -- real.CreateUsedCarsMasterData -- 
+     -- real.CreateCarsMasterData -- 
+     -- real.CreateCarDetails --
+     -- real.CreateLocations --
+     -- real.CreateListings --
+    """
+    stored_procedure = "real.CreateUsedCarsMasterData"
